0199f2b639a57720285df82f20c351d1c2f78f52
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-09-04 11:03:05 +1000

    Install crash-safe stability runtime

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from .runtime_fixes_compat import install as _install_runtime_fixes
 from .scene_presence_runtime import install as _install_scene_presence
 from .game_day import install as _install_game_day
+from .stability_runtime import install as _install_stability_runtime
 
 _install_runtime_fixes()
 _install_scene_presence()
 _install_game_day()
+_install_stability_runtime()
 del _install_runtime_fixes
 del _install_scene_presence
 del _install_game_day
+del _install_stability_runtime
