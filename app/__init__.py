916c5c90447f1f671784c396c91d7670cad0cb17
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-09-09 01:33:06 +1000

    Install story thread runtime before writer context

--- app/__init__.py
+++ app/__init__.py
@@ -9,6 +9,7 @@ from .memory_integrity_runtime import install as _install_memory_integrity_runti
 from .character_chunk_read import install as _install_character_chunk_read
 from .transport_scope_runtime import install as _install_transport_scope_runtime
 from .npc_intent_runtime import install as _install_npc_intent_runtime
+from .story_thread_runtime import install as _install_story_thread_runtime
 from .writer_first_runtime import install as _install_writer_first_runtime
 from .narrative_guardrails_runtime import install as _install_narrative_guardrails_runtime
 from .fast_audit_runtime import install as _install_fast_audit_runtime
@@ -25,6 +26,7 @@ _install_memory_integrity_runtime()
 _install_character_chunk_read()
 _install_transport_scope_runtime()
 _install_npc_intent_runtime()
+_install_story_thread_runtime()
 _install_writer_first_runtime()
 _install_narrative_guardrails_runtime()
 _install_fast_audit_runtime()
@@ -40,6 +42,7 @@ del _install_memory_integrity_runtime
 del _install_character_chunk_read
 del _install_transport_scope_runtime
 del _install_npc_intent_runtime
+del _install_story_thread_runtime
 del _install_writer_first_runtime
 del _install_narrative_guardrails_runtime
 del _install_fast_audit_runtime
