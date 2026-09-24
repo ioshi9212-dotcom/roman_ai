7fd512a16d40e6c03c13489a58af8f394c88b876
ioshi9212-dotcom <ioshi9212@gmail.com>
2026-09-24 19:13:43 +1000

    setup v5: install fixed-profile setup flow

--- app/__init__.py
+++ app/__init__.py
@@ -20,6 +20,7 @@ from .draft_intake_runtime import install as _install_draft_intake_runtime
 from .cast_registry_runtime import install as _install_cast_registry_runtime
 from .relationship_policy_runtime import install as _install_relationship_policy_runtime
 from .setup_draft_v3_runtime import install as _install_setup_draft_v3_runtime
+from .simple_setup_runtime import install as _install_simple_setup_runtime
 from .knowledge_firewall_runtime import install as _install_knowledge_firewall_runtime
 
 _install_runtime_fixes()
@@ -44,6 +45,7 @@ _install_draft_intake_runtime()
 _install_cast_registry_runtime()
 _install_relationship_policy_runtime()
 _install_setup_draft_v3_runtime()
+_install_simple_setup_runtime()
 _install_knowledge_firewall_runtime()
 del _install_runtime_fixes
 del _install_foundation_coverage_runtime
@@ -67,4 +69,5 @@ del _install_draft_intake_runtime
 del _install_cast_registry_runtime
 del _install_relationship_policy_runtime
 del _install_setup_draft_v3_runtime
+del _install_simple_setup_runtime
 del _install_knowledge_firewall_runtime
